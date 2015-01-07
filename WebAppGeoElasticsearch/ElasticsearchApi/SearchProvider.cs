15adc4bcd47ae84246e864695ea5e54fc2e16219
damienbod <damien_bod@hotmail.com>
2015-01-07 23:28:40 +0100

    tip fix

--- WebAppGeoElasticsearch/ElasticsearchApi/SearchProvider.cs
+++ WebAppGeoElasticsearch/ElasticsearchApi/SearchProvider.cs
@@ -11,7 +11,7 @@ namespace WebAppGeoElasticsearch.ElasticsearchApi
 	public class SearchProvider
 	{
 		private readonly IElasticsearchMappingResolver _elasticsearchMappingResolver = new ElasticsearchMappingResolver();
-		private const string ConnectionString = "http://localhost.fiddler:9200";
+		private const string ConnectionString = "http://localhost:9200";
 
 		public void InitMapDetailMapping()
 		{
@@ -63,7 +63,7 @@ namespace WebAppGeoElasticsearch.ElasticsearchApi
 		//			}
 		//		}
 		//	]
-		//	},
+		//	}
 		//}
 		public List<MapDetail> SearchForClosest(int maxDistanceInMeter, double centerLongitude, double centerLatitude)
 		{
